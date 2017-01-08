cafd1a7e2a248bdaee71783cdb52990df523f5c9
Yoan-dev <yoanbocquelet@gmail.com>
2017-01-08 15:30:02 +0100

    IhmAdapter

---
+++ src/v2/IhmAdapter.java
@@ -0,0 +1,48 @@
+package v2;
+
+import shared.Command.Command;
+import shared.IHM.Ihm;
+import shared.IHM.IhmEvent;
+
+public class IhmAdapter implements Ihm {
+
+    @Override
+    public void updateTempo(double tempo) {
+
+    }
+
+    @Override
+    public void updateStarted(boolean started) {
+
+    }
+
+    @Override
+    public void updateTime(int time) {
+
+    }
+
+    @Override
+    public double getCursorPosition() {
+        return 0;
+    }
+
+    @Override
+    public void tempo() {
+
+    }
+
+    @Override
+    public void mesure() {
+
+    }
+
+    @Override
+    public void switchLed(String type) {
+
+    }
+
+    @Override
+    public void setCommand(IhmEvent type, Command c) {
+
+    }
+}
