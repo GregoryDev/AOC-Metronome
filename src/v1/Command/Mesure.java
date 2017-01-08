4c80ba7d5270604368ea9d8dd7f7a57e6759d091
Yoan-dev <yoanbocquelet@gmail.com>
2017-01-08 12:05:45 +0100

    update commands

--- src/v1/Command/Mesure.java
+++ src/v1/Command/Mesure.java
@@ -1,12 +1,9 @@
 package v1.Command;
 
-/**
- * Created by yoan on 08/01/17.
- */
 public class Mesure extends Command {
 
     @Override
     public void execute() {
-        controller.incTime();
+        controller.mesure();
     }
 }
