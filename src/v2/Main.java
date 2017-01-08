33561ea13893db6777f88422210cd756f4d6fa21
Grégory <greg.pennetier@gmail.com>
2017-01-08 15:25:12 +0100

    main and fxml v2

---
+++ src/v2/Main.java
@@ -0,0 +1,16 @@
+package v2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
