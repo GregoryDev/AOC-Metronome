71d589beb145b1f705e9b8ae9017565055f8ef0b
Grégory <greg.pennetier@gmail.com>
2017-01-08 15:29:17 +0100

    main v2

--- src/v2/Main.java
+++ src/v2/Main.java
@@ -1,13 +1,35 @@
 package v2;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import shared.Controller.ControllerImp;
+import shared.Engine.EngineImp;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxml = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = fxml.load();
+        primaryStage.setTitle("Metronome-v2");
+        primaryStage.setScene(new Scene(root, 600, 300));
+        primaryStage.show();
 
+        init(fxml);
+
+        primaryStage.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+    }
+
+    public void init(FXMLLoader fxml) {
+        new ControllerImp(new EngineImp(), fxml.getController());
     }
 
     public static void main(String[] args) {
