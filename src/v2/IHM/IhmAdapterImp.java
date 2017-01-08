384534bfd284cb58cfbbe2f0ab3db647f9a79f17
Yoan-dev <yoanbocquelet@gmail.com>
2017-01-08 18:26:31 +0100

    solution cursor adapter

--- src/v2/IHM/IhmAdapterImp.java
+++ src/v2/IHM/IhmAdapterImp.java
@@ -55,15 +55,15 @@ public class IhmAdapterImp implements Ihm, IhmAdapter, Initializable {
         keysCommands.put(KeyCode.M, IhmEvent.DEC);
         keysCommands.put(KeyCode.SPACE, IhmEvent.START);
         keysCommands.put(KeyCode.BACK_SPACE, IhmEvent.STOP);
-        keysCommands.put(KeyCode.DOLLAR, IhmEvent.CURSOR);
-        keysCommands.put(KeyCode.CIRCUMFLEX, IhmEvent.CURSOR);
+        keysCommands.put(KeyCode.O, IhmEvent.CURSOR);
+        keysCommands.put(KeyCode.L, IhmEvent.CURSOR);
 
         keysStates.put(KeyCode.P, false);
         keysStates.put(KeyCode.M, false);
         keysStates.put(KeyCode.SPACE, false);
         keysStates.put(KeyCode.BACK_SPACE, false);
-        keysStates.put(KeyCode.DOLLAR, false);
-        keysStates.put(KeyCode.CIRCUMFLEX, false);
+        keysStates.put(KeyCode.O, false);
+        keysStates.put(KeyCode.L, false);
     }
 
     @Override
@@ -116,9 +116,16 @@ public class IhmAdapterImp implements Ihm, IhmAdapter, Initializable {
     @Override
     public void read() {
         for (KeyCode key : keysStates.keySet()) {
-            if (keysStates.get(key) && !keyboard.containsKey(key))
+            if (keysStates.get(key) && !keyboard.containsKey(key)) {
+                updateCursor(key);
                 commands.get(keysCommands.get(key)).execute();
+            }
             keysStates.put(key, keyboard.containsKey(key));
         }
+    }
+
+    private void updateCursor(KeyCode key) {
+        if (key == KeyCode.O) cursorValue = Math.min(300, cursorValue + 10);
+        if (key == KeyCode.L) cursorValue = Math.max(10, cursorValue - 10);
     }
 }
